a48055e4619613cdeba5b553ed5827d69ee4fdcc
Toshiaki Wakabayashi <genzouw@gmail.com>
2023-04-30 08:19:48 +0900

    Add to output a default env variable.

--- .github/scripts/use_environment_variables.js
+++ .github/scripts/use_environment_variables.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 console.log("START------");
+console.log(process.env.GITHUB_ACTOR);
 console.log(process.env.POSTGRES_HOST);
 console.log(process.env.POSTGRES_PORT);
 console.log("END------");
